43e8747eb731732a2c385a555f2b72edd3f1d9aa
Your Name <you@example.com>
2022-01-25 16:41:52 -0600

    UI implementada, regreso el bug del sonido :(

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from pynput import keyboard
 import threading
 from playsound import playsound
 import os
+import PySimpleGUI as sg
 
 global state,pointsInScreen,sound,mouse
 mouse = Controller()
@@ -12,26 +13,16 @@ pointsInScreen = []
 state = 0
 sound = 0
 
-print('')
-print('<Ctrl>   ---> Guarda la cordenada donde se hara un click')
-print('')
-print('<Shift>  ---> Empezar/Pausar/reanudar el script')
-print('')
-print('<Alt>    ---> Salir del programa')
-print('')
-print('Una vez iniciada la secuencia tu mouse brincará de cordenada en cordenada')
-print('por obvias razones se recomienda pausar el programa (Shift + p)')
-print('o terminar el programa (Alt) cuando se necesite hacer algo con el mouse')
-
+def printInUi(txt=None):
+    if txt:
+        window['displayText'].update(txt)
 def resource_path(relative_path):
     if hasattr(sys, '_MEIPASS'):
         return os.path.join(sys._MEIPASS, relative_path)
     return os.path.join(os.path.abspath("."), relative_path)
-
 def playSound(snd):
     if sound:
         playsound(resource_path(snd))
-
 def false_Click():
     global state, pointsInScreen
     for pos in pointsInScreen:
@@ -42,48 +33,61 @@ def false_Click():
         mouse.press(Button.left)
         playSound('click.wav')
         mouse.release(Button.left)
-        print('click en : {0}'.format(pos))
+        printInUi('Clicked on: {0}'.format(pos))
     if state == 1:
         false_Click()
-
 def record_click():
     global state, pointsInScreen
     if state == 0:
         playSound('confirmation.wav')
         pointsInScreen.append(mouse.position)
-        print('cordenada agregada: {}'.format(mouse.position))
-
-def start_click(message='comenzar el script'):
+        printInUi('Coord added: {}'.format(mouse.position))
+def start_click(message='Starting task'):
     global state, pointsInScreen
     if state == 0:
         if pointsInScreen:
-            print(message)
+            printInUi(message)
             playSound('resume.wav')
             state = 1
             thread = threading.Thread(target=false_Click)
             thread.start()
     elif state == 2:
         state = 0
-        start_click('reanudar')
+        start_click('Resume')
     elif state == 1:
         state = 2
-        print('pausa')
+        printInUi('Pause')
         playSound('pause.wav')
-
 def reset_click():
     global state, pointsInScreen
     if state != 0:
         state = 0
         pointsInScreen = []
-        print('Reset de clicks aplicado')
+        printInUi('Coords reset successfully')
         playSound('pause.wav')
-
 def end():
-    print('adios')
     sys.exit()
 
+#//////////////////////////////////////
+#//////////// UI //////////////////////
+#//////////////////////////////////////
+
+# Define the window's contents
+layout = [[sg.Text('Press Ctrl',key='displayText')],[sg.Button('Start'),sg.Button('Reset'),sg.Button('Quit')] ]
+# Create the window
+window = sg.Window('Autoclick', layout,icon=resource_path('favicon.ico'), return_keyboard_events=True,use_default_focus=True)
+# working main window
 with keyboard.GlobalHotKeys({
-        '<ctrl>': record_click,
-        '<shift>': start_click,
-        '<alt>': end}) as s:
+    '<ctrl>': record_click,
+    '<shift>': start_click}) as s:
+    while True:
+        event, values = window.read()
+        # See if user wants to quit or window was closed
+        if event == sg.WINDOW_CLOSED or event == 'Quit':
+            end()
+        elif event == 'Start':
+            start_click()
+        elif event == 'Reset':
+            reset_click()
     s.join()
+window.close()
