470946d35d5eeebaf3950d63185985f9ae2b4026
maxir143 <maxi.rodrigues.143@gmail.com>
2022-01-24 20:17:09 -0600

    debuguear audios

--- main.py
+++ main.py
@@ -6,36 +6,30 @@ import threading
 from playsound import playsound
 from pathlib import Path
 
-
 global state,pointsInScreen,sound,mouse
 mouse = Controller()
 pointsInScreen = []
 state = 0
-sound = 1
-
-
 
 print('CONTROLES :')
 print('')
-print('Ctrl ---> Guarda la cordenada donde se hara un click')
+print('<Ctrl>       ---> Guarda la cordenada donde se hara un click')
 print('')
-print('Shift + S ---> Empezas el script')
+print('<Shift>      ---> Empezar/Pausar/reanudar el script')
 print('')
-print('Shift + p ---> Pausar/reanudar el script ')
+print('<shift> + r  ---> Borrar coordenadas previas')
 print('')
-print('Shift + R ---> Borrar coordenadas previas')
-print('')
-print('Alt  ---> Salir del programa:')
+print('<Alt>        ---> Salir del programa:')
 print('')
 print('Una vez iniciada la secuencia tu mouse brincará de cordenada en cordenada')
 print('por obvias razones se recomienda pausar el programa (Shift + p)')
 print('o terminar el programa (Alt) cuando se necesite hacer algo con el mouse')
 
-def playSound(snd): #FALTA IMPLEMENTAR SONIDOS
-    path = Path(snd)
-    path = path.absolute()
-    #print(path)
-    #playsound(path)
+def playSound(snd):
+    path = Path('sounds')
+    path = path.absolute() / snd
+    print(path)
+    playsound(path)
 
 def false_Click():
     global state, pointsInScreen
@@ -45,6 +39,7 @@ def false_Click():
             break
         mouse.position = pos
         mouse.press(Button.left)
+        playSound('click.wav')
         mouse.release(Button.left)
         print('click en : {0}'.format(pos))
     if state == 1:
@@ -53,7 +48,7 @@ def false_Click():
 def record_click():
     global state, pointsInScreen
     if state == 0:
-        playSound('click.wav')
+        playSound('confirmation.wav')
         pointsInScreen.append(mouse.position)
         print('cordenada agregada: {}'.format(mouse.position))
 
@@ -62,25 +57,26 @@ def start_click():
     if state == 0:
         if pointsInScreen:
             print('comenzar el script')
+            playSound('resume.wav')
             state = 1
             thread = threading.Thread(target=false_Click)
             thread.start()
-
-def reset_click():
-    global state, pointsInScreen
-    state = 0
-    pointsInScreen = []
-    print('Reset de clicks aplicado')
-
-def pause_click():
-    global state, pointsInScreen
-    if state == 2:
-        print('no pausita')
+    elif state == 2:
+        print('reanudar')
         state = 0
         start_click()
     elif state == 1:
         state = 2
-        print('pausita')
+        print('pausa')
+        playSound('pause.wav')
+
+def reset_click():
+    global state, pointsInScreen
+    if state != 0:
+        state = 0
+        pointsInScreen = []
+        print('Reset de clicks aplicado')
+        playSound('pause.wav')
 
 def end():
     print('adios')
@@ -89,8 +85,6 @@ def end():
 with keyboard.GlobalHotKeys({
         '<ctrl>': record_click,
         '<shift>+s': start_click,
-        '<shift>+p': pause_click,
         '<shift>+r': reset_click,
-        '<alt>': end}) as l:
-    l.join()
-
+        '<alt>': end}) as s:
+    s.join()
