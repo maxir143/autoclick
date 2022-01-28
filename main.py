1da67d6ee9bcba1c2755ac0a45bca04a69ffd6b6
Your Name <you@example.com>
2022-01-27 18:04:43 -0600

    Ui: historial de cordenadas

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ sound = 0
 
 def printInUi(txt=None):
     if txt:
-        window['displayText'].update(txt)
+        window['display_text'].update(txt)
 def resource_path(relative_path):
     if hasattr(sys, '_MEIPASS'):
         return os.path.join(sys._MEIPASS, relative_path)
@@ -23,11 +23,12 @@ def resource_path(relative_path):
 def playSound(snd):
     if sound:
         playsound(resource_path(snd))
+
 def false_Click():
     global state, pointsInScreen
     for pos in pointsInScreen:
         time.sleep(2)
-        if state == 2:
+        if state == 2 or state == 0:
             break
         mouse.position = pos
         mouse.press(Button.left)
@@ -36,12 +37,25 @@ def false_Click():
         printInUi('Clicked on: {0}'.format(pos))
     if state == 1:
         false_Click()
+
 def record_click():
     global state, pointsInScreen
     if state == 0:
         playSound('confirmation.wav')
         pointsInScreen.append(mouse.position)
+        updateCordListUi(pointsInScreen)
         printInUi('Coord added: {}'.format(mouse.position))
+        focusUi()
+
+
+def updateCordListUi(cords):
+    window["cords_list"].update(cords)
+    print(cords)
+
+def focusUi():
+    window.BringToFront()
+
+
 def start_click(message='Starting task'):
     global state, pointsInScreen
     if state == 0:
@@ -56,15 +70,17 @@ def start_click(message='Starting task'):
         start_click('Resume')
     elif state == 1:
         state = 2
+        focusUi()
         printInUi('Pause')
         playSound('pause.wav')
+
 def reset_click():
     global state, pointsInScreen
-    if state != 0:
-        state = 0
-        pointsInScreen = []
-        printInUi('Coords reset successfully')
-        playSound('pause.wav')
+    state = 0
+    pointsInScreen = []
+    updateCordListUi(pointsInScreen)
+    printInUi('Coords reset successfully')
+    playSound('pause.wav')
 def end():
     sys.exit()
 
@@ -73,10 +89,11 @@ def end():
 #//////////////////////////////////////
 
 # Define the window's contents
-layout = [[sg.Text('Press Ctrl',key='displayText')],[sg.Button('Start'),sg.Button('Reset'),sg.Button('Quit')] ]
+layout = [[sg.Text('Press Ctrl',key='display_text')],[sg.Button('Start',key='btn_start'),sg.Button('Reset',key='btn_reset'),sg.Button('Quit')],[sg.Listbox([],size=(25, 10), enable_events=True, key='cords_list')]]
 # Create the window
-window = sg.Window('Autoclick', layout,icon=resource_path('favicon.ico'), return_keyboard_events=True,use_default_focus=True)
+window = sg.Window('Autoclick', layout,size=(220, 200),icon=resource_path('favicon.ico'), return_keyboard_events=True,use_default_focus=True)
 # working main window
+
 with keyboard.GlobalHotKeys({
     '<ctrl>': record_click,
     '<shift>': start_click}) as s:
@@ -85,9 +102,10 @@ with keyboard.GlobalHotKeys({
         # See if user wants to quit or window was closed
         if event == sg.WINDOW_CLOSED or event == 'Quit':
             end()
-        elif event == 'Start':
+        elif event == 'btn_start':
             start_click()
-        elif event == 'Reset':
+        elif event == 'btn_reset':
             reset_click()
+        window.TKroot.focus_force()
     s.join()
 window.close()
